14e6ce31961ff6d13a0088253753163c16ca3e1b
mepro789 <mis-is@meprofarm.com>
2025-09-16 09:55:29 +0700

    changes unit test

--- src/test/java/com/mepro/appname/ApplicationTests.java
+++ src/test/java/com/mepro/appname/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.mepro.ediscountmon;
+package com.mepro.appname;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EdiscountMonitoringApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
